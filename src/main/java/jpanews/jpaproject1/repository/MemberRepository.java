5afc9182e3231e40063a8b3502c396f9440c939b
Jason Sejik Lee <gggabikr@gmail.com>
2022-09-15 03:09:25 +0900

    Update MemberRepository.java

--- src/main/java/jpanews/jpaproject1/repository/MemberRepository.java
+++ src/main/java/jpanews/jpaproject1/repository/MemberRepository.java
@@ -1,22 +1,34 @@
 package jpanews.jpaproject1.repository;
 
 import jpanews.jpaproject1.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext
-    private EntityManager em;
+    private final EntityManager em;
 
     public Long save(Member member){
         em.persist(member);
         return member.getId();
     }
+
     public Member findOne(Long id){
         return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+    public List<Member> findByUsername(String username){
+        return em.createQuery("select m from Member m where m.username = :username", Member.class)
+                .setParameter("username", username)
+                .getResultList();
     }
 }
