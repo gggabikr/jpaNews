cad4d848ac0501554f781dd8b7334d3899996efe
Jason Sejik Lee <gggabikr@gmail.com>
2022-09-15 00:23:38 +0900

    Delete EngType.java

--- src/main/java/jpanews/jpaproject1/domain/EngType.java
+++
@@ -1,8 +0,0 @@
-package jpanews.jpaproject1.domain;
-
-public enum EngType {
-    NOUN, VERB, ADJECTIVE, ADVERB,
-    PRONOUN, DETERMINER, PREPOSITION,
-    CONJUNCTION, INTERJECTION
-    //명사, 동사, 형용사, 부사, 대명사, 한정사, 전치사, 접속사, 감탄사
-}
