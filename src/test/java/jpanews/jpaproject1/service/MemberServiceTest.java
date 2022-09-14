2fdb5ac534b745aedac58d97bbc6c4567b780f18
Jason Sejik Lee <gggabikr@gmail.com>
2022-09-15 03:09:27 +0900

    Create MemberServiceTest.java

---
+++ src/test/java/jpanews/jpaproject1/service/MemberServiceTest.java
@@ -0,0 +1,57 @@
+package jpanews.jpaproject1.service;
+
+import jpanews.jpaproject1.domain.Member;
+import jpanews.jpaproject1.repository.MemberRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    public void JoinMember() throws Exception{
+        //given
+        Member member = new Member();
+        member.setUsername("JasonLee");
+
+        //when
+        Long savedId = memberService.join(member);
+
+        //then
+        assertEquals(member, memberRepository.findByUsername("JasonLee").get(0));
+        assertEquals(member, memberRepository.findOne(savedId));
+        assertEquals(member.getPassword(), memberRepository.findOne(savedId).getPassword());
+
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void exceptionForDuplicated() throws Exception{
+        //given
+        Member member1 = new Member();
+        Member member2 = new Member();
+
+        member1.setUsername("Breece");
+        member2.setUsername("Breece");
+
+//        error should not be arisen -> fail() method need to be worked
+//        member2.setUsername("Breece2");
+
+        //when
+        memberService.join(member1);
+        memberService.join(member2); //error should be arisen
+
+        //then
+        fail("error must be arisen");
+    }
+}
