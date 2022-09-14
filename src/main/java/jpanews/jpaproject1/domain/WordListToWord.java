4987fe553c38e00316b1b1cb395ba0c9487880e1
Jason Sejik Lee <gggabikr@gmail.com>
2022-09-15 02:13:17 +0900

    Update WordListToWord.java

--- src/main/java/jpanews/jpaproject1/domain/WordListToWord.java
+++ src/main/java/jpanews/jpaproject1/domain/WordListToWord.java
@@ -1,6 +1,9 @@
 package jpanews.jpaproject1.domain;
 
+import jpanews.jpaproject1.domain.Words.Word;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -8,18 +11,24 @@ import java.time.LocalDateTime;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class WordListToWord {
 
     @Id @GeneratedValue
     @Column(name="ww_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "wordlist_id")
     private WordList wordList;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "word_id")
     private Word word;
 
     private LocalDateTime addDate;
 
+    private boolean status;
+
+    private int failedCount;
 }
