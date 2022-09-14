16e6ae0bfbe22e07a87a7f43538c6f5e79bb1dff
Jason Sejik Lee <gggabikr@gmail.com>
2022-09-15 02:13:21 +0900

    Create EngWord.java

---
+++ src/main/java/jpanews/jpaproject1/domain/Words/EngWord.java
@@ -0,0 +1,14 @@
+package jpanews.jpaproject1.domain.Words;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@Getter @Setter
+@DiscriminatorValue("E")
+public class EngWord extends Word{
+    private String eMeaning;
+}
