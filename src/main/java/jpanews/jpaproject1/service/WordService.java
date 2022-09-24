c9e7068e2f7cb21e2272fa71e7ecc1f6d10b87f1
Jason Sejik Lee <gggabikr@gmail.com>
2022-09-25 02:48:36 +0900

    Update WordService.java

--- src/main/java/jpanews/jpaproject1/service/WordService.java
+++ src/main/java/jpanews/jpaproject1/service/WordService.java
@@ -1,4 +1,83 @@
 package jpanews.jpaproject1.service;
 
+import jpanews.jpaproject1.domain.Words.Word;
+import jpanews.jpaproject1.repository.WordRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class WordService {
+
+    private final WordRepository wordRepository;
+
+    @Transactional
+    public void saveWordToDb(Word word) throws Exception {
+        validateTooManyWords(word);
+        wordRepository.save(word);
+    }
+
+    private void validateTooManyWords(Word word) throws Exception {
+        List<Word> findWords = wordRepository.findByName(word.getName());
+        if (findWords.size() >= 10){
+            throw new IllegalStateException("There are too many data saved for this word.");
+        }
+    }
+
+    public Word findById(Long id){
+        return wordRepository.findOne(id);
+    }
+
+    public List<Word> findAllWords(){
+        return wordRepository.findAll();
+    }
+
+    public List<Word> findAllKorWords(){
+        return wordRepository.findAllKorWord();
+    }
+
+    public List<Word> findAllEngWords(){
+        return wordRepository.findAllEngWord();
+    }
+
+    public List<Word> findWithString(String str) throws Exception{
+        int length = str.length();
+
+        boolean ifFirstCharIsStar = String.valueOf(str.charAt(0)) == "*";
+        boolean ifLastCharIsStar = String.valueOf(str.charAt(length - 1)) == "*";
+
+        List<Word> wordsNameStartingWith = wordRepository.findByNameStartingWith(str.substring(0, length - 1));
+        List<Word> wordsNameEndingWith = wordRepository.findByNameEndingWith(str.substring(1));
+        List<Word> exactMatch = wordRepository.findByName(str);
+        List<Word> containWords = wordRepository.findByNameContaining(str);
+
+        if(str.matches("[a-zA-Z]+")){   //e.g) 'vacation'
+            containWords.removeAll(exactMatch);
+            exactMatch.addAll(containWords);
+            return exactMatch;
+        } else if(ifFirstCharIsStar && ifLastCharIsStar){   //e.g) '*cat*'
+            containWords.removeAll(wordsNameStartingWith);
+            containWords.removeAll(wordsNameEndingWith);
+            return containWords;
+        } else if(ifFirstCharIsStar){   //e.g) '*tion'
+            return wordsNameEndingWith;
+        } else if(ifLastCharIsStar){    //e.g) 'pre*'
+            return wordsNameStartingWith;
+        } else {    //e.g) 'pre*tion'
+            ArrayList<Word> finalWords = new ArrayList<Word>();
+            for (Word wordA:wordsNameEndingWith) {
+                for (Word wordB:wordsNameStartingWith) {
+                    if(wordA.equals(wordB)){
+                        finalWords.add(wordA);
+                    }
+                }
+            }
+            return finalWords;
+        }
+    }
 }
