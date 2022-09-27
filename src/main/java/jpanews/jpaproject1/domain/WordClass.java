ffaf601a61503315eee1d3db922077d4fba3bef9
Jason Sejik Lee <gggabikr@gmail.com>
2022-09-27 21:07:45 +0900

    Update WordClass.java

--- src/main/java/jpanews/jpaproject1/domain/WordClass.java
+++ src/main/java/jpanews/jpaproject1/domain/WordClass.java
@@ -1,8 +1,22 @@
 package jpanews.jpaproject1.domain;
 
 public enum WordClass {
-    NOUN, VERB, ADJECTIVE, ADVERB,
-    PRONOUN, DETERMINER, PREPOSITION,
-    CONJUNCTION, INTERJECTION
+
+
+    NOUN("NOUN"), VERB("VERB"), ADJECTIVE("ADJECTIVE"),
+    ADVERB("ADVERB"), PRONOUN("PRONOUN"), DETERMINER("DETERMINER"),
+    PREPOSITION("PREPOSITION"),CONJUNCTION("CONJUNCTION"),INTERJECTION("INTERJECTION");
     //명사, 동사, 형용사, 부사, 대명사, 한정사, 전치사, 접속사, 감탄사
+
+    private final String text;
+
+
+    WordClass(final String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return text;
+    }
 }
