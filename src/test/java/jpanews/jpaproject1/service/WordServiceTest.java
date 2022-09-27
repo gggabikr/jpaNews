acedca1c541b7a90d1b8193e36ae1a07efa93d13
Jason Sejik Lee <gggabikr@gmail.com>
2022-09-28 02:05:19 +0900

    Update WordServiceTest.java

--- src/test/java/jpanews/jpaproject1/service/WordServiceTest.java
+++ src/test/java/jpanews/jpaproject1/service/WordServiceTest.java
@@ -1,6 +1,7 @@
 package jpanews.jpaproject1.service;
 
 import jpanews.jpaproject1.domain.WordClass;
+import jpanews.jpaproject1.domain.Words.EngWord;
 import jpanews.jpaproject1.domain.Words.KorWord;
 import jpanews.jpaproject1.domain.Words.Word;
 import jpanews.jpaproject1.repository.WordRepository;
@@ -45,70 +46,104 @@ public class WordServiceTest {
         wordService.saveWordToDb(word2);
 
         //then
-        Assertions.assertEquals(word1, wordRepository.findOne(word1.getId()));
-        Assertions.assertEquals(1, wordRepository.findByName("pool").size());
-        Assertions.assertEquals(word1.getName(), wordRepository.findByName("pool").get(0).getName());
-        Assertions.assertEquals("NOUN", wordRepository.findAllKorWord().get(0).getWordClass().toString());
+        Assertions.assertEquals(word1, wordService.findById(word1.getId()));
+        Assertions.assertEquals(1, wordService.findWithString("pool").size());
+        Assertions.assertEquals(word1.getName(), wordService.findWithString("pool").get(0).getName());
+        Assertions.assertEquals("NOUN", wordService.findWithString("pool").get(0).getWordClass().toString());
 
         List<Word> list = new ArrayList<>();
         list.add(word1);
         list.add(word2);
-        Assertions.assertEquals(list, wordRepository.findAllKorWord());
-        Assertions.assertEquals(list, wordRepository.findAll());
-        Assertions.assertEquals(list, wordRepository.findByWordClass("NOUN"));
-        Assertions.assertEquals(list, wordRepository.findByWordClass("Noun"));
-        Assertions.assertEquals(list, wordRepository.findByWordClass("nOUn"));
-        Assertions.assertEquals(list, wordRepository.findByWordClass("noun"));
+
+        Assertions.assertEquals(list, wordService.findAllKorWords());
+        Assertions.assertEquals(list, wordService.findAllWords());
+        Assertions.assertEquals(list, wordService.findWithWordClass("NOUN"));
+        Assertions.assertEquals(list, wordService.findWithWordClass("Noun"));
+        Assertions.assertEquals(list, wordService.findWithWordClass("nOUn"));
+        Assertions.assertEquals(list, wordService.findWithWordClass("noun"));
 
     }
 
     @Test
-    public void findById() {
+    public void findById() throws Exception {
         //given
+        KorWord word1 = new KorWord();
+        word1.setName("pool");
+        word1.setKMeaning("바보");
+        word1.setWordClass(WordClass.NOUN);
 
+        KorWord word2 = new KorWord();
+        word2.setName("cat");
+        word2.setKMeaning("고양이");
+        word2.setWordClass(WordClass.NOUN);
 
         //when
-
+        wordService.saveWordToDb(word1);
+        wordService.saveWordToDb(word2);
 
         //then
-
+        Assertions.assertEquals(word1, wordService.findById(word1.getId()));
+        Assertions.assertEquals(word2, wordService.findById(word2.getId()));
     }
 
     @Test
-    public void findAllWords() {
+    public void findAllWords() throws Exception {
         //given
+        KorWord word1 = new KorWord();
+        word1.setName("pool");
+        word1.setKMeaning("바보");
+        word1.setWordClass(WordClass.NOUN);
 
+        KorWord word2 = new KorWord();
+        word2.setName("cat");
+        word2.setKMeaning("고양이");
+        word2.setWordClass(WordClass.NOUN);
 
+        EngWord word3 = new EngWord();
+        word3.setName("great");
+        word3.setEMeaning("aaaaaaaaaa");
+        word3.setWordClass(WordClass.ADJECTIVE);
         //when
-
+        wordService.saveWordToDb(word1);
+        wordService.saveWordToDb(word2);
+        wordService.saveWordToDb(word3);
 
         //then
 
+        //test for findAllWords
+        Assertions.assertEquals(word1, wordService.findAllWords().get(0));
+        Assertions.assertEquals(word2, wordService.findAllWords().get(1));
+        Assertions.assertEquals(word3, wordService.findAllWords().get(2));
+
+        Assertions.assertEquals(3, wordService.findAllWords().size());
+        Assertions.assertEquals(word2.getWordClass(), wordService.findAllWords().get(1).getWordClass());
+        Assertions.assertEquals(word3.getWordClass(), wordService.findAllWords().get(2).getWordClass());
+
+        //test for findAllKorWords
+        Assertions.assertEquals(word1, wordService.findAllKorWords().get(0));
+        Assertions.assertEquals(word2, wordService.findAllKorWords().get(1));
+        Assertions.assertEquals(2, wordService.findAllKorWords().size());
+        Assertions.assertEquals(word2.getWordClass(), wordService.findAllKorWords().get(1).getWordClass());
+
+        //test for findAllEngWords
+        Assertions.assertEquals(word3, wordService.findAllEngWords().get(0));
+        Assertions.assertEquals(1, wordService.findAllEngWords().size());
+        Assertions.assertEquals(word3.getWordClass(), wordService.findAllEngWords().get(0).getWordClass());
+        Assertions.assertEquals(word3.getMeaning(), wordService.findAllEngWords().get(0).getMeaning());
+        Assertions.assertEquals(word3.getEMeaning(), wordService.findAllEngWords().get(0).getMeaning());
+
+        //test for findWithWordClass
+        Assertions.assertEquals(2, wordService.findWithWordClass("Noun").size());
+        Assertions.assertEquals(2, wordService.findWithWordClass("NoUn").size());
+        Assertions.assertEquals(word1, wordService.findWithWordClass("NoUn").get(0));
+        Assertions.assertEquals(word2, wordService.findWithWordClass("nOun").get(1));
+
+        Assertions.assertEquals(1, wordService.findWithWordClass("Adjective").size());
+        Assertions.assertEquals(word3.getMeaning(), wordService.findWithWordClass("adjecTIve").get(0).getMeaning());
+
     }
 
-    @Test
-    public void findAllKorWords() {
-        //given
 
-
-        //when
-
-
-        //then
-
-    }
-
-    @Test
-    public void findAllEngWords() {
-        //given
-
-
-        //when
-
-
-        //then
-
-    }
 
     @Test
     public void findWithString() {
