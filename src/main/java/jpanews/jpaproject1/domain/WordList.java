47de650170324576f65babe0af7443e62d56ad47
Jason Sejik Lee <gggabikr@gmail.com>
2022-10-03 17:11:53 +0900

    Update WordList.java

--- src/main/java/jpanews/jpaproject1/domain/WordList.java
+++ src/main/java/jpanews/jpaproject1/domain/WordList.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Getter @Setter
+@Getter
 public class WordList {
 
     @Id @GeneratedValue
@@ -21,4 +21,43 @@ public class WordList {
 
     @OneToMany(mappedBy = "wordList", cascade = CascadeType.ALL)
     private List<WordListToWord> wordListToWords = new ArrayList<>();
+
+    //finish rate
+
+    private int numerator; //분자
+
+    private int denominator; //분모
+
+
+    //==relational methods==//
+    public void setMember(Member member){
+        this.member = member;
+        member.getWordLists().add(this);
+    }
+
+    public void saveWordListToWord(WordListToWord wordListToWord){
+        this.getWordListToWords().add(wordListToWord);
+        wordListToWord.setWordList(this);
+    }
+
+
+    //constructor
+    public static WordList createWordList(Member member, WordListToWord... wordListToWords){
+        WordList wordList = new WordList();
+        wordList.setMember(member);
+
+        for(WordListToWord wordListToWord : wordListToWords){
+            wordList.saveWordListToWord(wordListToWord);
+        }
+        wordList.denominator = wordList.getWordListToWords().size();
+        wordList.numerator = 0;
+        return wordList;
+    }
+
+    public void deleteWordList(){
+        for (WordListToWord wordListToWord : this.wordListToWords) {
+            wordListToWord.delete();
+        }
+
+    }
 }
