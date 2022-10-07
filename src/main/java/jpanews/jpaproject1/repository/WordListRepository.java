f53058a32bdb69c3f57799a28687ad026ffed675
Jason Sejik Lee <gggabikr@gmail.com>
2022-10-07 17:05:37 +0900

    Update WordListRepository.java

--- src/main/java/jpanews/jpaproject1/repository/WordListRepository.java
+++ src/main/java/jpanews/jpaproject1/repository/WordListRepository.java
@@ -1,10 +1,12 @@
 package jpanews.jpaproject1.repository;
 
 import jpanews.jpaproject1.domain.WordList;
+import jpanews.jpaproject1.domain.Words.Word;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -18,5 +20,24 @@ public class WordListRepository {
 
     public WordList findOne(Long id){
         return em.find(WordList.class, id);
+    }
+
+    public List<WordList> findAllByMember(Long memberId){
+        return em.createQuery(
+                        "SELECT wl FROM WordList wl WHERE wl.member = :memberId", WordList.class)
+                .setParameter("memberId", memberId)
+                .getResultList();
+    }
+
+//    //혹은 위 메서드에 Lang 변수 넣는식으로 하고 하나 없애기
+//    public List<WordList> findAllEngByMember(Long memberId){
+//    }
+//    public List<WordList> findAllKorByMember(Long memberId) {
+//    }
+
+    public List<WordList> findAllByMemorizedStatus(Long memberId, int percent){
+        return em.createQuery("SELECT wl FROM WordList wl WHERE wl.numerator/wl.denominator < :percent AND wl.member = :memberId",WordList.class)
+                .setParameter("percent",percent).setParameter("memberId", memberId)
+                .getResultList();
     }
 }
