530412c81f5e923b1eb1218dca7318548e835c89
Jason Sejik Lee <gggabikr@gmail.com>
2022-10-09 18:58:06 +0900

    Update WordRepository.java

--- src/main/java/jpanews/jpaproject1/repository/WordRepository.java
+++ src/main/java/jpanews/jpaproject1/repository/WordRepository.java
@@ -1,11 +1,11 @@
 package jpanews.jpaproject1.repository;
 
-import jpanews.jpaproject1.domain.WordList;
 import jpanews.jpaproject1.domain.Words.Word;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -80,13 +80,6 @@ public class WordRepository {
 //        occurTooShortInputException(wordClass);
         return em.createQuery("SELECT w FROM Word w WHERE UPPER(w.wordClass) LIKE UPPER(TRIM(:wordClass))", Word.class)
                 .setParameter("wordClass", wordClass)
-                .getResultList();
-    }
-
-    public List<Word> RandomSelect(Long wordListId, int number) {
-        return em.createQuery("SELECT wlw FROM WordListToWord wlw WHERE wlw.id = :wordListId ORDER BY RAND()",Word.class)
-                .setParameter("wordListId", wordListId)
-                .setMaxResults(number)
                 .getResultList();
     }
 
