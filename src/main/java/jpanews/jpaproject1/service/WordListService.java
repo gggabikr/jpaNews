1ef503d30917308804f3b8d67e7974525adf901c
Jason Sejik Lee <gggabikr@gmail.com>
2022-10-09 23:34:30 +0900

    Update WordListService.java

--- src/main/java/jpanews/jpaproject1/service/WordListService.java
+++ src/main/java/jpanews/jpaproject1/service/WordListService.java
@@ -13,7 +13,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Collections;
 
 @Service
 @RequiredArgsConstructor
@@ -25,6 +27,7 @@ public class WordListService {
     private final MemberRepository memberRepository;
 
     //==create WordList==//
+    @Transactional
     public Long createWordList(Long memberId, List<Long> wordIdList) {
 
         //Refer entities
@@ -39,5 +42,38 @@ public class WordListService {
         WordList newWordList = WordList.createNewWordList(member, wlws);
 
         return newWordList.getId();
+    }
+
+    //==delete WordList==//
+    @Transactional
+    public void deleteWordList(Long wordListId){
+        WordList wordList = wordListRepository.findOne(wordListId);
+        for(WordListToWord wlw: wordList.getWordListToWords()){
+            wlw.delete();
+        }
+    }
+
+    //==test==//
+    @Transactional
+    public String testWords(Long wordListId,int numOfWords) throws Exception {
+
+        List<WordListToWord> randomSelectedWlws
+                = wlwRepository.RandomSelect(wordListId, numOfWords);
+
+        HashMap<Integer, List<String>> hashMapOfTestQ = new HashMap<>();
+
+        for (WordListToWord wlw : randomSelectedWlws) {
+            List<String> answers = new ArrayList<>();
+            String rightAnswer = wlw.getWord().getMeaning();
+            answers.add(rightAnswer);
+            List<Word> byWordClass = wordRepository.findByWordClass(String.valueOf(wlw.getWord().getWordClass()));
+            for (int i = 0; i<3; i++){
+                String wrongAnswer = byWordClass.get((int) (Math.random() * byWordClass.size())).getMeaning();
+                answers.add(wrongAnswer);
+            }
+            Collections.shuffle(answers);
+            hashMapOfTestQ.put(answers.indexOf(rightAnswer),answers);
+        }
+
     }
 }
