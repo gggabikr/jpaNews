1b01401a27a13eeee1a7fedb449f2d3df151f754
Jason Sejik Lee <gggabikr@gmail.com>
2022-10-09 18:57:57 +0900

    Create WordListToWordRepository.java

---
+++ src/main/java/jpanews/jpaproject1/repository/WordListToWordRepository.java
@@ -0,0 +1,48 @@
+package jpanews.jpaproject1.repository;
+
+import jpanews.jpaproject1.domain.WordListToWord;
+import jpanews.jpaproject1.domain.Words.Word;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class WordListToWordRepository {
+
+    private final EntityManager em;
+
+//    public void addWLW(WordListToWord wlw){
+//        em.persist(wlw);
+//    }
+
+
+
+    public List<WordListToWord> RandomSelect(Long wordListId, int number) {
+        List<WordListToWord> selectedWordList = em.createQuery("SELECT wlw FROM WordListToWord wlw WHERE wlw.id = :wordListId", WordListToWord.class)
+                .setParameter("wordListId", wordListId)
+                .getResultList();
+        int qty = selectedWordList.size();
+        if(number>qty){
+            number = qty;
+        }
+
+        ArrayList<Integer> selectedNumbers = new ArrayList<>();
+
+        while(selectedNumbers.size()<number){
+            int randomNumber = (int)(Math.random() * qty);
+            if(!selectedNumbers.contains(randomNumber)){
+                selectedNumbers.add(randomNumber);
+            }
+        }
+        ArrayList<WordListToWord> selectedWordListToWords = new ArrayList<>();
+        for (int index:selectedNumbers) {
+            selectedWordListToWords.add(selectedWordList.get(index));
+        }
+
+        return selectedWordListToWords;
+    }
+}
