ea3700ade4a5c94adcbc2e10fc0c34eedd2793f9
Jason Sejik Lee <gggabikr@gmail.com>
2022-10-12 21:17:06 +0900

    Create WordListServiceTest.java

---
+++ src/test/java/jpanews/jpaproject1/service/WordListServiceTest.java
@@ -0,0 +1,87 @@
+package jpanews.jpaproject1.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class WordListServiceTest {
+
+    @Test
+    public void createWordList() throws Exception{
+        //given
+
+
+        //when
+
+
+        //then
+
+
+    }
+
+
+    @Test
+    public void testCreateWordList() throws Exception{
+        //given
+
+
+        //when
+
+
+        //then
+
+
+    }
+
+    @Test
+    public void deleteWordList() throws Exception{
+        //given
+
+
+        //when
+
+
+        //then
+
+
+    }
+
+    @Test
+    public void makeAnswerList() throws Exception{
+        //given
+
+
+        //when
+
+
+        //then
+
+
+    }
+
+    @Test
+    public void checkRightOrWrong() throws Exception{
+        //given
+
+
+        //when
+
+
+        //then
+
+
+    }
+
+    @Test
+    public void testWords() throws Exception{
+        //given
+
+
+        //when
+
+
+        //then
+
+
+    }
+}
