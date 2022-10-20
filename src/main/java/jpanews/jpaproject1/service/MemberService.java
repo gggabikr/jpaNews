a98e821bc087ca5aa8266a61d43ee24ee694c28a
Jason Sejik Lee <gggabikr@gmail.com>
2022-10-20 22:23:16 +0900

    Update MemberService.java

--- src/main/java/jpanews/jpaproject1/service/MemberService.java
+++ src/main/java/jpanews/jpaproject1/service/MemberService.java
@@ -40,10 +40,6 @@ public class MemberService {
     }
 
     //비번 최소 길이, 최대길이, 소문자 대문자 등등 체크하는 기능 넣기
-    //이거 비번 바꾼후에 save할때 업데이트 제대로 되는지
-    // (save할때 아이디가 이미 있는 아이디라면~~ 아니면~~ 하는 방법 JPAShop에 있었던걸로 기억)
-    //그리고 테스트 하기. (비번이 바뀌었는지,새비번으로 로그인 되는지, 이전비번으로 로그인 실패하는지,
-    // 가입후 워드리스트, wlw등 생성하고, 그 후 비번 바꿈. 기존것들 다 연동되어있는지 체크등등.)
 
     @Transactional
     public Long changePassword(Long memberId, String newPassword) throws Exception {
