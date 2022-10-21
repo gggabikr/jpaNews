59cd792b5f2804437225863d22e17c520af0921e
Jason Sejik Lee <gggabikr@gmail.com>
2022-10-21 23:57:18 +0900

    Create CustomWordListToWordRepository.java

---
+++ src/main/java/jpanews/jpaproject1/repository/CustomWordListToWordRepository.java
@@ -0,0 +1,16 @@
+package jpanews.jpaproject1.repository;
+
+import jpanews.jpaproject1.domain.WordListToWord;
+import java.util.List;
+
+public interface CustomWordListToWordRepository {
+
+    public List<WordListToWord> findAll();
+
+    public List<WordListToWord> findAllByWordList(Long wordListId);
+
+
+    public List<WordListToWord> RandomSelect(Long wordListId, int number);
+
+    public void deleteWlw(Long wlwId);
+}
