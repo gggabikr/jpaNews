f54a2fd7bcfa9e3ef78bc3d3eb80ea47eadac081
Jason Sejik Lee <gggabikr@gmail.com>
2022-10-21 23:57:33 +0900

    Update WordListToWordRepository.java

--- src/main/java/jpanews/jpaproject1/repository/WordListToWordRepository.java
+++ src/main/java/jpanews/jpaproject1/repository/WordListToWordRepository.java
@@ -1,65 +1,17 @@
 package jpanews.jpaproject1.repository;
 
+import jpanews.jpaproject1.domain.WordList;
 import jpanews.jpaproject1.domain.WordListToWord;
-import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
-import java.util.ArrayList;
 import java.util.List;
 
 @Repository
-@RequiredArgsConstructor
-public class WordListToWordRepository {
+public interface WordListToWordRepository extends JpaRepository<WordListToWord, Long>, CustomWordListToWordRepository {
+    Page<WordListToWord> findAllByWordListIdOrderByAddDate(Long wordListId, Pageable pageable);
 
-    private final EntityManager em;
-
-//    public void addWLW(WordListToWord wlw){
-//        em.persist(wlw);
-//    }
-
-    public List<WordListToWord> findAll(){
-        return em.createQuery(
-                        "SELECT wlw FROM WordListToWord wlw", WordListToWord.class)
-                .getResultList();
-    }
-
-    public List<WordListToWord> findAllByWordList(Long wordListId){
-        return em.createQuery(
-                "SELECT wlw FROM WordListToWord wlw WHERE wlw.wordList.id=:wordListId",WordListToWord.class)
-                .setParameter("wordListId", wordListId)
-                .getResultList();
-    }
-
-
-    public List<WordListToWord> RandomSelect(Long wordListId, int number) {
-        List<WordListToWord> selectedWordList =
-                em.createQuery("SELECT wlw FROM WordListToWord wlw WHERE wlw.wordList.id = :wordListId", WordListToWord.class)
-                .setParameter("wordListId", wordListId)
-                .getResultList();
-        int qty = selectedWordList.size();
-
-        if(number>qty){
-            number = qty;
-        }
-
-        ArrayList<Integer> selectedNumbers = new ArrayList<>();
-
-        while(selectedNumbers.size()<number){
-            int randomNumber = (int)(Math.random() * qty);
-            if(!selectedNumbers.contains(randomNumber)){
-                selectedNumbers.add(randomNumber);
-            }
-        }
-        ArrayList<WordListToWord> selectedWordListToWords = new ArrayList<>();
-        for (int index:selectedNumbers) {
-            selectedWordListToWords.add(selectedWordList.get(index));
-        }
-
-        return selectedWordListToWords;
-    }
-
-    public void deleteWlw(Long wlwId){
-        em.remove(em.find(WordListToWord.class, wlwId));
-    }
+    List<WordListToWord> findAllByWordListIdOrderByIdDesc(Long wordListId, Pageable pageable);
 }
