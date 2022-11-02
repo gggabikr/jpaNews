93562fd848e35bac1d46c3441f03c6a26c8e3db3
Jason Sejik Lee <gggabikr@gmail.com>
2022-11-02 23:03:09 +0900

    Update Member.java

--- src/main/java/jpanews/jpaproject1/domain/Member.java
+++ src/main/java/jpanews/jpaproject1/domain/Member.java
@@ -2,8 +2,10 @@ package jpanews.jpaproject1.domain;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,6 +21,11 @@ public class Member {
 
     private String password;
 
+    private String role;
+
     @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
     private List<WordList> WordLists = new ArrayList<>();
+
+    @CreationTimestamp
+    private Timestamp createDate;
 }
