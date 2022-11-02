28d177c732d5faa705992bf8240b7b63457733d6
Jason Sejik Lee <gggabikr@gmail.com>
2022-11-03 00:01:33 +0900

    Create MemberInfoDto.java

---
+++ src/main/java/jpanews/jpaproject1/service/MemberInfoDto.java
@@ -0,0 +1,11 @@
+package jpanews.jpaproject1.service;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class MemberInfoDto {
+    private String username;
+    private String password;
+    private String role;
+}
