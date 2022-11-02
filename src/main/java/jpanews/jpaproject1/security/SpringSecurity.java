3f413d587a0e0b26977d4f7a633d021b326cb802
Jason Sejik Lee <gggabikr@gmail.com>
2022-11-02 23:03:12 +0900

    Update SpringSecurity.java

--- src/main/java/jpanews/jpaproject1/security/SpringSecurity.java
+++ src/main/java/jpanews/jpaproject1/security/SpringSecurity.java
@@ -2,6 +2,8 @@ package jpanews.jpaproject1.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -19,6 +21,19 @@ public class SpringSecurity extends WebSecurityConfigurerAdapter{
                 .csrf().disable()		//csrf방지
                 .formLogin().disable()	//기본 로그인 페이지 없애기
                 .headers().frameOptions().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder authManager) throws Exception {
+        // This is the code you usually have to configure your authentication manager.
+        // This configuration will be used by authenticationManagerBean() below.
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        // ALTHOUGH THIS SEEMS LIKE USELESS CODE,
+        // IT'S REQUIRED TO PREVENT SPRING BOOT AUTO-CONFIGURATION
+        return super.authenticationManagerBean();
     }
 
     @Bean
