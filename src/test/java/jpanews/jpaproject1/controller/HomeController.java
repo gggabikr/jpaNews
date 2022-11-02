a1ed65ce8fdfd6d1a57e6c0ce4aa9fc4010c5b7d
Jason Sejik Lee <gggabikr@gmail.com>
2022-11-02 22:53:52 +0900

    Create HomeController.java

---
+++ src/test/java/jpanews/jpaproject1/controller/HomeController.java
@@ -0,0 +1,19 @@
+package jpanews.jpaproject1.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+public class HomeController {
+
+    //아래와 같이 로거를 뽑을 수 있지만 롬복을 쓰면 어노테이션으로 가능하다.
+    //Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("/")
+    public String home(){
+        log.info("home controller");
+        return "home";
+    }
+}
