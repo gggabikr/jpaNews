b7ce96cf291d1c542180cd0b0edff66a9e1358d8
Jason Sejik Lee <gggabikr@gmail.com>
2022-11-07 23:07:10 +0900

    Update MemberInfoDto.java

--- src/main/java/jpanews/jpaproject1/service/MemberInfoDto.java
+++ src/main/java/jpanews/jpaproject1/service/MemberInfoDto.java
@@ -3,9 +3,14 @@ package jpanews.jpaproject1.service;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotEmpty;
+
 @Getter @Setter
 public class MemberInfoDto {
+    @NotEmpty(message = "You must enter the username.")
     private String username;
+    @NotEmpty(message = "You must enter the password.")
     private String password;
+    @NotEmpty(message = "You must choose the role.")
     private String role;
 }
