09fbb85340d9287eb39b0358e9ec47f1fd9b81f0
Jason Sejik Lee <gggabikr@gmail.com>
2022-11-10 00:39:16 +0900

    Create errorAlert.js

---
+++ src/main/resources/static/js/errorAlert.js
@@ -0,0 +1,5 @@
+function errorAlertUsernameDupl(){
+    // if(message && (typeof message === "string" || message instanceof String)){
+    //     let getParam = window.location.search.substring(1);
+        alert("Given username is in use. Please try again.")
+}
