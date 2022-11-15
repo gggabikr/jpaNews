6f6d2e9cc1c0493f249dd84e85b8350e7bc6ff3a
Jason Sejik Lee <gggabikr@gmail.com>
2022-11-15 11:42:46 +0900

    Update SpringSecurity - wrong injection modified

--- src/main/java/jpanews/jpaproject1/security/SpringSecurity.java
+++ src/main/java/jpanews/jpaproject1/security/SpringSecurity.java
@@ -1,18 +1,21 @@
 package jpanews.jpaproject1.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SpringSecurity extends WebSecurityConfigurerAdapter{
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -33,17 +36,15 @@ public class SpringSecurity extends WebSecurityConfigurerAdapter{
 
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder authManager) throws Exception {
-        // This is the code you usually have to configure your authentication manager.
-        // This configuration will be used by authenticationManagerBean() below.
-    }
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder authManager) throws Exception {
+//        // This is the code you usually have to configure your authentication manager.
+//        // This configuration will be used by authenticationManagerBean() below.
+//    }
 
     @Bean
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        // ALTHOUGH THIS SEEMS LIKE USELESS CODE,
-        // IT'S REQUIRED TO PREVENT SPRING BOOT AUTO-CONFIGURATION
-        return super.authenticationManagerBean();
+    public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
     @Bean
