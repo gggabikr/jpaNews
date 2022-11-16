81d3d66e10b223d81a5647a63f92ba5e7b200de5
Jason Sejik Lee <gggabikr@gmail.com>
2022-11-16 14:12:03 +0900

    Update articleCrawling.java

--- src/main/java/jpanews/jpaproject1/service/articleCrawling.java
+++ src/main/java/jpanews/jpaproject1/service/articleCrawling.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 
 public class articleCrawling {
 
-    String URL = "Please enter article's URL";
+//    String URL = "Please enter article's URL";
     Document doc;
 
     public boolean crawl(String url) {
@@ -18,9 +18,12 @@ public class articleCrawling {
 
         if (url.contains("abcnews")) {
             result = abcNews(url);
-        }
-        if(url.contains("bbc.com")){
+        } else if(url.contains("bbc.com")){
             result = bbcNews(url);
+        } else if(url.contains("nypost.com")){
+            return nyPost(url);
+        } else if(url.contains("msn.com") && url.contains("news")){
+            return msnNews(url);
         }
 
 
@@ -49,11 +52,11 @@ public class articleCrawling {
 
             for (Element element : body) {
                 System.out.println(element.text());
+                //ul, li 같은 리스트들을 어떻게 처리할지 고민해보자.
             }
         }
         return true;
     }
-
     public boolean nyTimes(String url){
         //because it requires paying to see the articles,
         //this method will not be built.
@@ -76,12 +79,38 @@ public class articleCrawling {
             System.out.println(title != null ? title.text() : null);
             for (Element element : body) {
                 System.out.println(element.text());
+                //ul, li 같은 리스트들을 어떻게 처리할지 고민해보자.
             }
         }
         return true;
     }
-//    public boolean nyPost(String url){}
-//    public boolean msnNews(String url){}
+    public boolean nyPost(String url){
+        try {
+            Connection conn = Jsoup.connect(url);
+            doc = conn.get();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+            System.out.println("Something went wrong.");
+            System.out.println("Please check the URL again.");
+            return false;
+        } finally {
+            Elements title = doc.getElementsByClass("headline--single");
+            Elements body = doc.select("#main>article>div>div>div>div>div>p");
+
+            System.out.println(title.size() > 0 ? title.text() : null);
+            for (Element element : body) {
+                System.out.println(element.text());
+                //ul, li 같은 리스트들을 어떻게 처리할지 고민해보자.
+            }
+        }
+        return true;
+        //#main>article>div>div>div>div>div>header>h1
+        //#main>article>div>div>div>div>div>p
+    }
+    public boolean msnNews(String url){
+        
+    }
 //    public boolean washingtonPost(String url){}
 //    public boolean cnnNews(String url){}
 //    public boolean googleNews(String url){}
@@ -93,14 +122,17 @@ public class articleCrawling {
 
 
     public static void main(String[] args) {
-        String URL = "https://abcnews.go.com/US/bus-carrying-18-students-driver-crashes-kentucky-multiple/story?id=93283274";
-        String URL2 = "https://www.bbc.com/news/world-middle-east-63636783";
-        String URL3 = "https://www.asded.com";
+        String abcUrl = "https://abcnews.go.com/US/bus-carrying-18-students-driver-crashes-kentucky-multiple/story?id=93283274";
+        String bbcUrl = "https://www.bbc.com/news/world-middle-east-63636783";
+        String nyPostUrl = "https://nypost.com/2022/11/15/jennifer-siebel-newsom-wife-to-california-gov-asked-to-fake-an-orgasm-in-court-during-harvey-weinstein-trial/";
+        String NotExistUrl = "https://www.asded.com";
+
         articleCrawling articleCrawling = new articleCrawling();
-//        articleCrawling.crawl(URL);
-        articleCrawling.crawl(URL2);
+//        articleCrawling.crawl(abcUrl);
+//        articleCrawling.crawl(bbcUrl);
 //        articleCrawling.crawl(URL3);
-//        articleCrawling.crawl(URL3);
+        articleCrawling.crawl(nyPostUrl);
+//        articleCrawling.crawl(NotExistUrl);
 
 
     }
