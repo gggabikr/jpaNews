fab5543810e25ab04870db9e5a61af9fffb4c37e
Jason Sejik Lee <gggabikr@gmail.com>
2022-11-24 22:20:24 -0800

    Update articleCrawling.java

--- src/main/java/jpanews/jpaproject1/service/articleCrawling.java
+++ src/main/java/jpanews/jpaproject1/service/articleCrawling.java
@@ -105,12 +105,32 @@ public class articleCrawling {
             }
         }
         return true;
-        //#main>article>div>div>div>div>div>header>h1
-        //#main>article>div>div>div>div>div>p
     }
     public boolean msnNews(String url){
-        
+        try {
+            Connection conn = Jsoup.connect(url);
+            doc = conn.get();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+            System.out.println("Something went wrong.");
+            System.out.println("Please check the URL again.");
+            return false;
+        } finally {
+            Elements title = doc.getElementsByTag("h1");
+            Element body = doc.getElementsByAttributeValue("role", "main").select("section").get(0);
+            System.out.println(title.size() > 0 ? title.text() : "could not find title");
+
+            String text = body.text();
+            int i = text.indexOf("Continue Reading Show full articles");
+            text = text.substring(0,i);
+
+            System.out.println(text);
+            //ul, li 같은 리스트들을 어떻게 처리할지 고민해보자.
+        }
+        return true;
     }
+//    will be updated later.
 //    public boolean washingtonPost(String url){}
 //    public boolean cnnNews(String url){}
 //    public boolean googleNews(String url){}
@@ -125,13 +145,17 @@ public class articleCrawling {
         String abcUrl = "https://abcnews.go.com/US/bus-carrying-18-students-driver-crashes-kentucky-multiple/story?id=93283274";
         String bbcUrl = "https://www.bbc.com/news/world-middle-east-63636783";
         String nyPostUrl = "https://nypost.com/2022/11/15/jennifer-siebel-newsom-wife-to-california-gov-asked-to-fake-an-orgasm-in-court-during-harvey-weinstein-trial/";
+        String msnUrl = "https://www.msn.com/en-us/news/technology/stranded-without-food-edible-drone-has-snackable-wings/ar-AA14991Z?ocid=EMMX&cvid=ec757b745bec4026aead2bea7d76f899";
+
+
         String NotExistUrl = "https://www.asded.com";
 
         articleCrawling articleCrawling = new articleCrawling();
 //        articleCrawling.crawl(abcUrl);
 //        articleCrawling.crawl(bbcUrl);
-//        articleCrawling.crawl(URL3);
-        articleCrawling.crawl(nyPostUrl);
+//        articleCrawling.crawl(nyPostUrl);
+        articleCrawling.crawl(msnUrl);
+
 //        articleCrawling.crawl(NotExistUrl);
 
 
