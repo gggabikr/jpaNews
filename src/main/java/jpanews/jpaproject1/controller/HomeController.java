5ed4ce8e91459a02f240153b0d96b0d3ac7c0580
Jason Sejik Lee <gggabikr@gmail.com>
2022-11-27 00:27:16 -0800

    Update HomeController.java

--- src/main/java/jpanews/jpaproject1/controller/HomeController.java
+++ src/main/java/jpanews/jpaproject1/controller/HomeController.java
@@ -1,13 +1,22 @@
 package jpanews.jpaproject1.controller;
 
+import jpanews.jpaproject1.service.WordListService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.security.Principal;
 
 @Controller
 @Slf4j
+@RequiredArgsConstructor
 public class HomeController {
-
+    private final WordListService wordListService;
     //아래와 같이 로거를 뽑을 수 있지만 롬복을 쓰면 어노테이션으로 가능하다.
     //Logger log = LoggerFactory.getLogger(getClass());
 
@@ -16,4 +25,18 @@ public class HomeController {
         log.info("home controller");
         return "home";
     }
+
+    @RequestMapping(value = "/username", method = RequestMethod.GET)
+    @ResponseBody
+    public String currentUserName(Principal principal) {
+        return principal.getName();
+    }
+
+    @GetMapping("/user/wordList")
+    public String ToWordList(Model model){
+        wordListService.findAllWordListByMember()
+        model.addAttribute("wordList", );
+        return "wordListPage";
+    }
+
 }
