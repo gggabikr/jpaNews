0dc1a88c2ada252208b4072d15997c79de6c12db
Jason Sejik Lee <gggabikr@gmail.com>
2022-11-27 23:26:45 -0800

    Update HomeController.java

--- src/main/java/jpanews/jpaproject1/controller/HomeController.java
+++ src/main/java/jpanews/jpaproject1/controller/HomeController.java
@@ -1,22 +1,25 @@
 package jpanews.jpaproject1.controller;
 
+import jpanews.jpaproject1.domain.Member;
+import jpanews.jpaproject1.service.MemberService;
 import jpanews.jpaproject1.service.WordListService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.security.Principal;
 
 @Controller
 @Slf4j
 @RequiredArgsConstructor
 public class HomeController {
     private final WordListService wordListService;
+    private final MemberService memberService;
+
+
     //아래와 같이 로거를 뽑을 수 있지만 롬복을 쓰면 어노테이션으로 가능하다.
     //Logger log = LoggerFactory.getLogger(getClass());
 
@@ -26,16 +29,26 @@ public class HomeController {
         return "home";
     }
 
-    @RequestMapping(value = "/username", method = RequestMethod.GET)
-    @ResponseBody
-    public String currentUserName(Principal principal) {
-        return principal.getName();
-    }
+//    //유저 이름 가져오는거 테스트용
+//    @RequestMapping(value = "/username", method = RequestMethod.GET)
+//    @ResponseBody
+//    public String currentUserName(Principal principal) {
+//        return principal.getName();
+//    }
 
     @GetMapping("/user/wordList")
-    public String ToWordList(Model model){
-        wordListService.findAllWordListByMember()
-        model.addAttribute("wordList", );
+//        public String ToWordList(Model model, @AuthenticationPrincipal Principal principal){
+        public String ToWordList(Model model){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        System.out.println(authentication.getName()); // == username
+        Member member = memberService.findByUsername(authentication.getName()).get(0);
+        System.out.println("member's name: " + member.getUsername());
+        System.out.println("member's role: " + member.getRole());
+        System.out.println("member's id: " + member.getId());
+
+        model.addAttribute("member", member);
+//        wordListService.findAllWordListByMember();
+//        model.addAttribute("wordList", );
         return "wordListPage";
     }
 
