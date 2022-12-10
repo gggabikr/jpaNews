8836ba63d9d89a189a5d78462938c4f7b1e7868f
Jason Sejik Lee <gggabikr@gmail.com>
2022-12-10 01:25:09 -0800

    Update uploadWordDataIntoDB.java

--- src/main/java/jpanews/jpaproject1/uploadWordDataIntoDB.java
+++ src/main/java/jpanews/jpaproject1/uploadWordDataIntoDB.java
@@ -1,5 +1,6 @@
 package jpanews.jpaproject1;
 
+import jpanews.jpaproject1.domain.WordClass;
 import jpanews.jpaproject1.domain.Words.EngWord;
 import jpanews.jpaproject1.domain.Words.Word;
 import jpanews.jpaproject1.repository.WordRepository;
@@ -26,7 +27,7 @@ public class uploadWordDataIntoDB {
                 int indexOfBracketOpen = line.indexOf('(');
                 int indexOfBracketClose = line.indexOf(')');
 //                System.out.println(indexOfBracketOpen + "," + indexOfBracketClose);
-                tempArr[0] = line.substring(0, indexOfBracketOpen);
+                tempArr[0] = line.substring(0, indexOfBracketOpen).trim();
 //                System.out.println(tempArr[0]);
 
                 if(indexOfBracketOpen+1 == indexOfBracketClose){
@@ -34,15 +35,39 @@ public class uploadWordDataIntoDB {
                     tempArr[1] = "n/a";
                 }else{
 //                    System.out.println(line.substring(indexOfBracketOpen+1, indexOfBracketClose-1));
-                    tempArr[1] = line.substring(indexOfBracketOpen+1, indexOfBracketClose-1);
+                    tempArr[1] = line.substring(indexOfBracketOpen+1, indexOfBracketClose-1).trim();
                 }
-                tempArr[2] = line.substring(indexOfBracketClose+1);
-                System.out.println(Arrays.toString(tempArr));
+                tempArr[2] = line.substring(indexOfBracketClose+1).trim();
+//                System.out.println(Arrays.toString(tempArr));
                 Word word = new EngWord();
                 word.setName(tempArr[0]);
-                case 
-                word.setWordClass(tempArr[1]);
+//                switch(tempArr[1]){
+//                    case x= "n":
+//                        continue;
+//                    case "v":
+//                    case "v. t.":
+//                    case "v. i":
+//                        continue;
+//                    case "a":
+//                        continue;
+//                }
 
+
+                if(tempArr[1].contains("imp") || tempArr[1].contains("p. p") || tempArr[1].length() == 0 || tempArr[1].equals("n/a")){
+                    word.setWordClass(WordClass.NOTABAILABLE);
+                } else if (tempArr[1].equals("n")){
+                    word.setWordClass(WordClass.NOUN);
+                } else if (tempArr[1].equals("v") || tempArr[1].contains("v.")){
+                    word.setWordClass(WordClass.VERB);
+                } else if (tempArr[1].equals("adv")){
+                    word.setWordClass(WordClass.ADVERB);
+                } else if (tempArr[1].equals("a")){
+                    word.setWordClass(WordClass.ADJECTIVE);
+                } //pl.? superl? adv. & a? p. pr. & vb. n? imp? 등등
+//                word.setWordClass(tempArr[1]);
+                System.out.println(word.getName() + ", " +word.getWordClass());
+                //null 값이 종종 있다. n.pl. 이라는 값을 가진것 포함.
+                // 이런경우는 이걸 또 하나의 배열로 봐야하나.....
 //                System.out.println(line.substring(indexOfBracketClose+1));//                for(String tempStr : tempArr) {
 //                    System.out.print(tempStr + "///");
 //                }
