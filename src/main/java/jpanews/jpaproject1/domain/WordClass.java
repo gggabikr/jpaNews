7d4661c2eee341a09476f87b20bf8d2bd8d297d4
Jason Sejik Lee <gggabikr@gmail.com>
2022-12-13 20:25:16 -0800

    Update WordClass.java

--- src/main/java/jpanews/jpaproject1/domain/WordClass.java
+++ src/main/java/jpanews/jpaproject1/domain/WordClass.java
@@ -5,8 +5,10 @@ public enum WordClass {
 
     NOUN("NOUN"), VERB("VERB"), ADJECTIVE("ADJECTIVE"),
     ADVERB("ADVERB"), PRONOUN("PRONOUN"), DETERMINER("DETERMINER"),
-    PREPOSITION("PREPOSITION"),CONJUNCTION("CONJUNCTION"),INTERJECTION("INTERJECTION");
-    //명사, 동사, 형용사, 부사, 대명사, 한정사, 전치사, 접속사, 감탄사
+    PREPOSITION("PREPOSITION"),CONJUNCTION("CONJUNCTION"),
+    INTERJECTION("INTERJECTION"), PLURAL("PLURAL"),
+    SUPERLATIVE("SUPERLATIVE"), PREFIX("PREFIX"), NOTABAILABLE("NOTABAILABLE");
+    //명사, 동사, 형용사, 부사, 대명사, 한정사, 전치사, 접속사, 감탄사, 복수형, 최상급, 접두사, 해당없음
 
     private final String text;
 
