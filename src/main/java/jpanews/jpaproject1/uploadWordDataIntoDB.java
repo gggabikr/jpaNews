1849634d6d810f150851cdd4afed81898cfed452
Jason Sejik Lee <gggabikr@gmail.com>
2022-12-13 20:25:14 -0800

    Update uploadWordDataIntoDB.java

--- src/main/java/jpanews/jpaproject1/uploadWordDataIntoDB.java
+++ src/main/java/jpanews/jpaproject1/uploadWordDataIntoDB.java
@@ -2,15 +2,15 @@ package jpanews.jpaproject1;
 
 import jpanews.jpaproject1.domain.WordClass;
 import jpanews.jpaproject1.domain.Words.EngWord;
-import jpanews.jpaproject1.domain.Words.Word;
+
 import jpanews.jpaproject1.repository.WordRepository;
-
+import lombok.RequiredArgsConstructor;
 import java.io.*;
-import java.util.Arrays;
 
+@RequiredArgsConstructor
 public class uploadWordDataIntoDB {
 
-    WordRepository wordRepository;
+    private final WordRepository wordRepository;
 
     public static void read(String csvFile) {
 //        final String delimiter = "[\\(\\)]";
@@ -36,49 +36,54 @@ public class uploadWordDataIntoDB {
                 }else{
 //                    System.out.println(line.substring(indexOfBracketOpen+1, indexOfBracketClose-1));
                     tempArr[1] = line.substring(indexOfBracketOpen+1, indexOfBracketClose-1).trim();
+                    if(tempArr[1].contains("&")){
+                        int i = tempArr[1].indexOf("&");
+                        tempArr[1] = tempArr[1].substring(0, i).trim();
+                    }
                 }
                 tempArr[2] = line.substring(indexOfBracketClose+1).trim();
 //                System.out.println(Arrays.toString(tempArr));
-                Word word = new EngWord();
+                EngWord word = new EngWord();
                 word.setName(tempArr[0]);
-//                switch(tempArr[1]){
-//                    case x= "n":
-//                        continue;
-//                    case "v":
-//                    case "v. t.":
-//                    case "v. i":
-//                        continue;
-//                    case "a":
-//                        continue;
-//                }
 
 
-                if(tempArr[1].contains("imp") || tempArr[1].contains("p. p") || tempArr[1].length() == 0 || tempArr[1].equals("n/a")){
+                if(tempArr[1].contains("imp") || tempArr[1].contains("p. p") || tempArr[1].contains("p pr")| tempArr[1].length() == 0 || tempArr[1].equals("n/a")){
                     word.setWordClass(WordClass.NOTABAILABLE);
-                } else if (tempArr[1].equals("n")){
+                } else if (tempArr[1].equals("n") || tempArr[1].equals("n.")){
                     word.setWordClass(WordClass.NOUN);
                 } else if (tempArr[1].equals("v") || tempArr[1].contains("v.")){
                     word.setWordClass(WordClass.VERB);
-                } else if (tempArr[1].equals("adv")){
+                } else if (tempArr[1].equals("adv") ||tempArr[1].equals("adv.")){
                     word.setWordClass(WordClass.ADVERB);
-                } else if (tempArr[1].equals("a")){
+                } else if(tempArr[1].equals("a") || tempArr[1].equals("a.")){
                     word.setWordClass(WordClass.ADJECTIVE);
-                } else if (tempArr[1].equals("pl.") || tempArr[1].contains("pl")){
+                } else if (tempArr[1].equals("pl.") || tempArr[1].equals("pl")){
                     word.setWordClass(WordClass.PLURAL);
-                } else if (tempArr[1].equals("prep")){
+                } else if (tempArr[1].equals("prep") || tempArr[1].equals("prep.")){
                     word.setWordClass(WordClass.PREPOSITION);
+                } else if (tempArr[1].contains("prefix") ||tempArr[1].equals("pref") ||tempArr[1].equals("pref.")){
+                    word.setWordClass(WordClass.PREFIX);
                 } else if (tempArr[1].contains("superl")){
                     word.setWordClass(WordClass.SUPERLATIVE);
+                } else if (tempArr[1].contains("interj")){
+                    word.setWordClass(WordClass.INTERJECTION);
+                } else if (tempArr[1].contains("prep")){
+                    word.setWordClass(WordClass.PREPOSITION);
+                } else if (tempArr[1].contains("pl")){
+                    word.setWordClass(WordClass.PLURAL);
+                } else if (tempArr[1].contains("adv")){
+                    word.setWordClass(WordClass.ADVERB);
+                } else if (tempArr[1].contains("a")){
+                    word.setWordClass(WordClass.ADJECTIVE);
+                } else if (tempArr[1].contains("n") || tempArr[1].contains("n.")){
+                    word.setWordClass(WordClass.NOUN);
+                } else{
+                    word.setWordClass(WordClass.NOTABAILABLE);
                 }
-                //pl.? superl? adv. & a? p. pr. & vb. n? imp? 등등
-//                word.setWordClass(tempArr[1]);
-                System.out.println(word.getName() + ", " +word.getWordClass());
-                //null 값이 종종 있다. n.pl. 이라는 값을 가진것 포함.
-                // 이런경우는 이걸 또 하나의 배열로 봐야하나.....
-//                System.out.println(line.substring(indexOfBracketClose+1));//                for(String tempStr : tempArr) {
-//                    System.out.print(tempStr + "///");
-//                }
-//                System.out.println();
+                word.setEMeaning(line.substring(indexOfBracketClose+1));
+//                wordRepository.save(word);
+//                System.out.println(word.getName() + ", " +word.getWordClass()
+//                        + ", " + word.getMeaning());
             }
             br.close();
         } catch(IOException ioe) {
