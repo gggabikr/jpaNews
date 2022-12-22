8347c71b16d5c0a92b3bfb75c1c6533d3aefd144
Jason Sejik Lee <gggabikr@gmail.com>
2022-12-22 00:05:50 -0800

    Update wordDto.java

--- src/main/java/jpanews/jpaproject1/service/wordDto.java
+++ src/main/java/jpanews/jpaproject1/service/wordDto.java
@@ -2,11 +2,14 @@ package jpanews.jpaproject1.service;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.lang.Nullable;
 
 import javax.validation.constraints.NotEmpty;
 
 @Getter @Setter
 public class wordDto {
+    @Nullable
+    private Long Id;
     @NotEmpty
     private String wordName;
     @NotEmpty
