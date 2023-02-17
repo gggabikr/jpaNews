03b14116a957f9b82545bc0afc28049ae63dbe7e
Jason Sejik Lee <gggabikr@gmail.com>
2023-02-16 19:01:58 -0800

    Update SearchController.java

--- src/main/java/jpanews/jpaproject1/controller/SearchController.java
+++ src/main/java/jpanews/jpaproject1/controller/SearchController.java
@@ -1,11 +1,13 @@
 package jpanews.jpaproject1.controller;
 
+import jpanews.jpaproject1.domain.Member;
+import jpanews.jpaproject1.domain.WordList;
 import jpanews.jpaproject1.domain.Words.Word;
-import jpanews.jpaproject1.service.WordService;
-import jpanews.jpaproject1.service.articleCrawling;
-import jpanews.jpaproject1.service.crawlingDto;
-import jpanews.jpaproject1.service.wordDto;
+import jpanews.jpaproject1.repository.CustomWordListToWordRepositoryImpl;
+import jpanews.jpaproject1.service.*;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +22,8 @@ public class SearchController {
 
     private final articleCrawling articleCrawling;
     private final WordService wordService;
+    private final WordListService wordListService;
+    private final MemberService memberService;
 
     @GetMapping("/searchURL")
     public String searchURL(Model model, String URL) throws Exception {
@@ -71,8 +75,30 @@ public class SearchController {
         model.addAttribute("ArticleSubtitle", dto.getSubTitle_each());
         model.addAttribute("ArticleBodyList", dto.getArticleBody());
         model.addAttribute("wordData", wordData);
+        getWordlists(model);
         //각단어마다 단어 찾아서 wordDto생성, arraylist of wordDto를넘기고
         //
         return "articleSearch";
+    }
+
+    private void getWordlists(Model model) {
+        getWordLists(model, memberService, wordListService);
+    }
+
+    static void getWordLists(Model model, MemberService memberService, WordListService wordListService) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        System.out.println("username: " + authentication.getName()); // == username
+        if (!authentication.getName().equals("anonymousUser")) {
+            Member member = memberService.findByUsername(authentication.getName()).get(0);
+            // System.out.println("member's name: " + member.getUsername());
+            // System.out.println("member's role: " + member.getRole());
+            // System.out.println("member's id: " + member.getId());
+            List<WordList> wordListByMember = wordListService.findAllWordListByMember(member.getId());
+            for (WordList wordList : wordListByMember) {
+                wordList.updateMemorizedStatus();
+            }
+            model.addAttribute("member", member);
+            model.addAttribute("wordLists", wordListByMember);
+        }
     }
 }
